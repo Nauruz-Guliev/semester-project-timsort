0b3da61fe1582b3d7ded1ea4dde657185a6a0201
nauruz <nauruz9248327@gmail.com>
2022-05-19 20:00:47 +0300

    added threads for better performance

--- src/ru/kpfu/itis/gnt/Test.java
+++ src/ru/kpfu/itis/gnt/Test.java
@@ -1,16 +1,25 @@
 package ru.kpfu.itis.gnt;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.stream.Stream;
 
 public class Test {
-    public static void main(String[] args) {
-
-        Sort sort = new TimSort();
-
-        int[] arr = new int[]{4, 100, 9, 2, 3, 1000, -19, 344, 6, 23, 8};
-        for (int i : arr) { System.out.print(i + " "); }
-        sort.sort(arr, 1, 5);
-        System.out.println("\n");
-        for (int i : arr) { System.out.print(i + " "); }
+    private static File mainFold;
+    public static void main(String[] args) throws IOException {
+        TimeTests test1 = new TimeTests(new File("src/testFiles"));
+        TimeTests test2 = new TimeTests(new File("src/testFiles"));
+        File results = new File("src/testFiles/Results/");
+        results.mkdirs();
+        //timsort
+        test1.setResults(new File(results.toPath().resolve("timsort.txt").toString()));
+        test1.setSort(new TimSort());
+        //mergesort
+        test2.setResults(new File(results.toPath().resolve("mergesort.txt").toString()));
+        test2.setSort(new MergeSort());
+        new Thread(test1).start();
+        new Thread(test2).start();
+        test2.timeTest();
+        test1.timeTest();
     }
 }
--- src/ru/kpfu/itis/gnt/TimeTests.java
+++ src/ru/kpfu/itis/gnt/TimeTests.java
@@ -5,37 +5,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class TimeTests {
+public class TimeTests implements Runnable{
+    private File results;
+    private Sort sort;
 
     public TimeTests(File mainFold) {
         this.mainFold = mainFold;
         this.mainFold.mkdir();
     }
-
+    @Override
+    public void run() {
+        try {
+            timeTest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static File mainFold;
-
     private static final int countOfTests = 10;
     private static final int k1 = 2; //коэффециент1 увеличения кол-ва элементов
     private static final int k2 = 5; //коэффециент2 увеличения кол-ва элементов
     private static final int countOfDataSets = 10;
     private static final int dataSetSize = (int) 1e6;
-
-
-    public static void main(String[] args) throws IOException {
-
-        TimeTests test = new TimeTests(new File("src/testFiles"));
-        //test.createDataSets("sort");
-
-
-
-        File results = new File(mainFold.toPath().resolve("Results").toString());
-        results.mkdir();
-//        File timsortResults = new File(results.toPath().resolve("timsort.txt").toString());
-//        test.TimeTest(timsortResults, new TimSort());
-        File mergesortResults = new File(results.toPath().resolve("mergesort.txt").toString());
-        test.TimeTest(mergesortResults, new MergeSort());
-    }
 
 
     public void createDataSets(String operation) throws IOException {
@@ -50,14 +42,15 @@ public class TimeTests {
         data.createNewFile();
         FileWriter out = new FileWriter(data);
         for (int i = 0; i < dataSetSize; i++) {
-            out.write((int)(Math.random()*1000000000) + " ");
+            out.write((int) (Math.random() * 1000000000) + " ");
         }
         out.flush();
         out.close();
     }
 
 
-    public void TimeTest(File results, Sort sort) throws IOException {
+
+    public void timeTest() throws IOException {
         int dataSetSizeCopy = dataSetSize;
         int size = -2;
         while (dataSetSizeCopy > 0) {
@@ -65,16 +58,17 @@ public class TimeTests {
             dataSetSizeCopy /= k1 * k2;
         }
         int[] times = new int[size];
-        int[] timesSizes = new int[size]; int ij = 0;
+        int[] timesSizes = new int[size];
+        int ij = 0;
         boolean bool = true;
-        for (int curDataSetSize = k2; curDataSetSize <= dataSetSize; curDataSetSize = bool ? curDataSetSize*k1 : curDataSetSize*k2, bool = !bool) {
+        for (int curDataSetSize = k2; curDataSetSize <= dataSetSize; curDataSetSize = bool ? curDataSetSize * k1 : curDataSetSize * k2, bool = !bool) {
             timesSizes[ij] = curDataSetSize;
-System.out.println(curDataSetSize + " dataSetSize");
+            System.out.println(curDataSetSize + " dataSetSize " + Thread.currentThread().getName());
             for (int i = 0; i < countOfDataSets; i++) {
-System.out.print("\tdataSetNumber: " + i + "; Test number: ");
+                System.out.print("\tdataSetNumber: " + i + "; Test number: ");
                 for (int j = 0; j < countOfTests; j++) {
-System.out.print(j + " ");
-                    Scanner data = new Scanner(new File(mainFold.toPath().resolve("sort").resolve( "dataSet" + i + ".txt").toString()));
+                    System.out.print(j + " ");
+                    Scanner data = new Scanner(new File(mainFold.toPath().resolve("sort").resolve("dataSet" + i + ".txt").toString()));
                     int[] arr = new int[curDataSetSize];
                     for (int l = 0; l < curDataSetSize; l++) {
                         arr[l] = data.nextInt();
@@ -87,7 +81,7 @@ System.out.print(j + " ");
                     times[ij] += t2 - t1;
 
                 }
-System.out.println();
+                System.out.println();
             }
             ij++;
         }
@@ -96,13 +90,18 @@ System.out.println();
         FileWriter out = new FileWriter(results);
         for (int j = 0; j < size; j++) {
             times[j] = times[j] / (countOfDataSets * countOfTests);
-            out.write(( timesSizes[j] + "\t" + times[j] + "\n").replace('.',','));
+            out.write((timesSizes[j] + "\t" + times[j] + "\n").replace('.', ','));
         }
         out.flush();
         out.close();
     }
 
 
+    public void setResults(File results) {
+        this.results = results;
+    }
 
-
+    public void setSort(Sort sort) {
+        this.sort = sort;
+    }
 }
